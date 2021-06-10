0b241f369d58688ddb9a4a5460e4d3e855e8466f
Emman Lijesta <emman.lijesta@gmail.com>
2021-06-11 02:33:46 +0800

    Update vernam.php

--- vernam.php
+++ vernam.php
@@ -2,66 +2,51 @@
 # By Engr. Emman Lijesta, ECE
 # www.kiddyyep.com
 # Vernam Cipher is an unbreakable encryption method,
-# as long as key is not discovered for decoding it =)
-# used for encoding/decoding plain texts
-
-# how to use it: vernam( $data, $key )
+# as long as the key or salt is not discovered for decoding it
+# used for encoding/decoding plain text
+#
+# how to use it: vernam( $text, $salt )
 # for encoding - vernam( "plain text" , "YOURKEYHERE" ) returns "CIPHERTEXT"
 # for decoding - vernam( "CIPHERTEXT" , "YOURKEYHERE" ) returns "plain text"
 
-# Iteration version of Vernam that is slower than recursive,
-# but without a call limit size and is great for huge data
-function vernamSlow($data, $key) {
-	# get string lengths
-	$dataLen = strlen($data);
-	$keyLen = strlen($key);
+function slow($text, $salt, $bytes) {
+	$textLen = strlen($text);
+	$saltLen = strlen($salt);
+	$textNew = str_split($text);
+	$len = -1;
 
-	# let output be an array of data to have same type and length
-	$output = $data;
-
-	# iterate from 0 to data length
-	for ($i = 0; $i < $dataLen; $i++) {
-		# the modulo generates a random ID from 0 to length of key
-		$output[$i] = $data[$i] ^ $key[$i % $keyLen];
+	if ($bytes <= 5000) {
+		# foreach is fast for 5000 characters and below
+		foreach( $textNew as $key=>$value ) {
+			$textNew[$key] = $value ^ $salt[$key % $saltLen];
+		}
+	} else {
+		# while is fast for 5000 characters and above, it's great for huge data
+		while ( ++$len < $textLen ) {
+			$textNew[$len] = $text[$len] ^ $salt[$len % $saltLen];
+		}
 	}
 
-	return $output;
+	return implode('', $textNew);
 }
 
-# recursive is faster but there is a call limit stack size
-# in PHP limited by RAM ex. 512MB for basic server plans
-function vernamFast($data, $key, $output = array()) {
-	# get string and array lengths
-	$dataLen = strlen($data);
-	$keyLen = strlen($key);
-	$outputLen = count($output);
-
-	# if less than data length then continue recursion
-	if ($outputLen < $dataLen) {
-		array_push($output, $data[$outputLen] ^ $key[$outputLen % $keyLen]);
-		return vernamFast($data, $key, $output);
-
-	} else {
-		# converts array to string
-		return implode('', $output);
-	}
+function fastSub($text, $salt, $textNew, $len, $textLen, $saltLen) {
+	# recursive function is faster but restricted by maximum stack size limit
+	$textNew[] = $text[$len] ^ $salt[$len % $saltLen];
+	return (++$len < $textLen) ? fastSub($text, $salt, $textNew, $len, $textLen, $saltLen) : implode('', $textNew);
 }
 
-# use both iteration and recursive method depending on the size of the string for performance
-# 1 character = 1 byte, 256 char = 256 bytes
-# for 512MB with 256 bytes will be able to handle 2M calls to this function
-function vernam($data, $key, $limit = 256) {
-	$output = "";
+function fast($text, $salt, $len = 0) {
+	# declare these variables once to avoid overheads
+	$textLen = strlen($text);
+	$saltLen = strlen($salt);
+	$textNew = array();
 
-	if (strlen($data) <= $limit) {
-		# recursive vernam
-		$output = vernamFast($data, $key);
+	# do recursion
+	return fastSub($text, $salt, $textNew, $len, $textLen, $saltLen);
+}
 
-	} else {
-		# iterate vernam
-		$output = vernamSlow($data, $key);
-	}
-	
-	return $output;
+function vernam($text, $salt, $bytes = 256) {
+	return (strlen($text) <= $bytes) ? fast($text, $salt) : slow($text, $salt, $bytes);
 }
 ?>
